ca57cab3b4672c9dc9d11540fed2dc06ed5e2cc0
mng2214 <your_email@example.com>
2024-03-03 15:05:27 -0600

    Spring Eager and Lazy

--- Spring-05-Core-BeanScope/src/main/java/com/icode/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/icode/service/CommentService.java
@@ -5,13 +5,15 @@ import com.icode.proxy.CommentNotificationProxy;
 import com.icode.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@AllArgsConstructor
 //@Scope("prototype")  // Will create different bean
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
 public class CommentService {
 
     private final CommentRepository commentRepository;
@@ -20,6 +22,7 @@ public class CommentService {
     public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Eager vs Lazy");
     }
 
     public void publishComment(Comment comment) {
