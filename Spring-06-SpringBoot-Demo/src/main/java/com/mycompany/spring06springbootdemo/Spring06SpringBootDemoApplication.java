8b30de01e7386d7099390001d9159d5d8836d31e
mng2214 <mng2214@gmail.com>
2024-03-08 19:23:32 -0600

    spring project creation

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/mycompany/spring06springbootdemo/Spring06SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.spring06springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring06SpringBootDemoApplication.class, args);
+	}
+
+}
