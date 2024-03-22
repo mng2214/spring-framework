5ed1d59dc548b34201c5e90e6eef654a2671d5ee
mng2214 <mng2214@gmail.com>
2024-03-21 20:09:04 -0500

    static added

--- Spring-09-Spring-Thymeleaf/src/main/java/com/company/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/company/controller/StudentController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("register")
+    @RequestMapping("/register")
     public String register(Model model) {
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
+    @RequestMapping("/welcome")
+    public String info (){
+        return "student/welcome";
+    }
+
+
 }
