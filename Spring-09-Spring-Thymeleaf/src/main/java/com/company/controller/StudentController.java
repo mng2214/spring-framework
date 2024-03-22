78e23fe52a9774eaf7abab6f10bcea0f46174975
mng2214 <mng2214@gmail.com>
2024-03-21 19:05:42 -0500

    thymeleaf module - thymeleaf review

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/company/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.company.controller;
+
+import com.company.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("register")
+    public String register(Model model) {
+        model.addAttribute("students", DataGenerator.createStudent());
+        return "student/register";
+    }
+}
