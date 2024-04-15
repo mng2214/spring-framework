48962b8b516bae6458eaf3afbee76be1cc311aa1
mng2214 <mng2214@gmail.com>
2024-04-14 22:13:22 -0500

    orm module created

---
+++ Spring-10-ORM-TableCreation/src/test/java/com/emp/spring10ormtablecreation/Spring10OrmTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.spring10ormtablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10OrmTableCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
