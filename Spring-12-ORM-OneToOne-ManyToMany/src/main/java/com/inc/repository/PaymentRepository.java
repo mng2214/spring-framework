3db8b7ebf5147350050538f6b1660777b3d7e136
mng2214 <mng2214@gmail.com>
2024-05-06 19:57:43 -0500

    14 module added queries

--- Spring-12-ORM-OneToOne-ManyToMany/src/main/java/com/inc/repository/PaymentRepository.java
+++ Spring-12-ORM-OneToOne-ManyToMany/src/main/java/com/inc/repository/PaymentRepository.java
@@ -3,5 +3,15 @@ package com.inc.repository;
 import com.inc.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    // method -> sql == derive query findById(int id);
+
+    //sql  @Query -> pure sql query
+
+    //JPQL @Query -> java persistence ql
+    
+
 }
