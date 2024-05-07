4fcce1fd6cb5ff000ce7448dedba4c5262232405
mng2214 <mng2214@gmail.com>
2024-05-06 20:59:42 -0500

    queries classes built

--- Spring-14-ORM-Queries/src/main/java/com/inc/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/inc/bootstrap/DataGenerator.java
@@ -1,10 +1,13 @@
 package com.inc.bootstrap;
 
 import com.inc.repository.RegionRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataGenerator implements Runnable {
+@Transactional
+public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
 
@@ -13,18 +16,23 @@ public class DataGenerator implements Runnable {
     }
 
     @Override
-    public void run() {
+    public void run(String... args) throws Exception {
 
         System.out.println("-------------------------Region Start-------------------------");
 
+        System.out.println("findByCountry" + regionRepository.findByCountry("Canada"));
 
+        System.out.println("findDistinctByCountry" + regionRepository.findDistinctByCountry("Canada"));
+
+        System.out.println("findByCountryContainingIgnoreCase" + regionRepository.findByCountryContainingIgnoreCase("United"));
+
+        System.out.println("findByCountryContainingOrderByCountry" + regionRepository.findByCountryContainingOrderByCountry("Asia"));
+
+        System.out.println("findTop2ByCountry" + regionRepository.findTop2ByCountry("Canada"));
 
         System.out.println("-------------------------Region End-------------------------");
-
-
-
-
     }
+
 
 
 }
--- Spring-14-ORM-Queries/src/main/java/com/inc/repository/RegionRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/inc/repository/RegionRepository.java
@@ -9,9 +9,19 @@ import java.util.List;
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
     // Display All Regions in Canada
-
     List<Region> findByCountry(String country);
 
+    // Display all regions in Canada without Dups
+    List<Region> findDistinctByCountry(String country);
+
+    // Display all regions with country name includes 'united'
+    List<Region> findByCountryContainingIgnoreCase(String regionName);
+
+    // Display all regions with country name includes in order
+    List<Region> findByCountryContainingOrderByCountry(String regionName);
+
+    // Display top 2 regions in Canada
+    List<Region> findTop2ByCountry(String regionName);
 
 
 }
