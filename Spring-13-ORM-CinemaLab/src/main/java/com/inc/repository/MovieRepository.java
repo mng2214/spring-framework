e1824e66e4c6bf1ab85fc54a66f61f40e701b8fb
mng2214 <mng2214@gmail.com>
2024-05-22 20:20:46 -0500

    queries practice

--- Spring-13-ORM-CinemaLab/src/main/java/com/inc/repository/MovieRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/inc/repository/MovieRepository.java
@@ -20,36 +20,46 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     Optional<Movie> findByName(String name);
 
     //Write a derived query to list all movies between a range of prices
-    List<Movie> getAllByPriceBetween(BigDecimal price, BigDecimal price2);
+    List<Movie> findAllByPriceBetween(BigDecimal priceStart, BigDecimal priceEnd);
 
+    //  10 , 20 , 25
+    //SELECT * FROM movie WHERE duration IN (10,20,25)
     //Write a derived query to list all movies where duration exists in the specific list of duration
     List<Movie> findAllByDurationIn(List<Integer> duration);
 
     //Write a derived query to list all movies with higher than a specific release date
-    List<Movie> getAllByReleaseDateAfter(LocalDate releaseDate);
+    List<Movie> findAllByReleaseDateAfter(LocalDate releaseDate);
 
     //Write a derived query to list all movies with a specific state and type
-    List<Movie> getAllByStateAndType(MovieState state, MovieType type);
+    List<Movie> findAllByStateAndType(MovieState state, MovieType type);
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to list all movies between a range of prices
-    @Query("SELECT m FROM Movie m WHERE m.price BETWEEN :low AND :high")
-    List<Movie> movieBetweenPriceRange(BigDecimal low, BigDecimal high);
+    @Query("SELECT m FROM Movie m WHERE m.price BETWEEN ?1 AND ?2")
+    List<Movie> getByPriceBetween(BigDecimal priceStart, BigDecimal priceEnd);
 
     //Write a JPQL query that returns all movie names
     @Query("SELECT m.name FROM Movie m")
-    List<String> listOfAllMovieNames();
+    List<String> fetchAllMovieNames();
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns a movie by name
-
+    @Query(value = "SELECT * FROM movie WHERE name = ?1", nativeQuery = true)
+    Optional<Movie> retrieveByName(String name);
 
     //Write a native query that return the list of movies in a specific range of prices
+    @Query(value = "SELECT * FROM movie WHERE price BETWEEN ?1 AND ?2", nativeQuery = true)
+    List<Movie> retrieveByPriceRange(BigDecimal price1, BigDecimal price2);
 
     //Write a native query to return all movies where duration exists in the range of duration
 
+    @Query(value = "SELECT * FROM movie WHERE duration IN ?1", nativeQuery = true)
+    List<Movie> retrieveByDurationInRange(List<Integer> durations);
+
     //Write a native query to list the top 5 most expensive movies
+    @Query(value = "SELECT * FROM movie ORDER BY price DESC LIMIT 5", nativeQuery = true)
+    List<Movie> top5ExpensiveMovies();
 
 }
--- Spring-13-ORM-CinemaLab/src/main/java/com/inc/repository/TicketRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/inc/repository/TicketRepository.java
@@ -1,29 +1,43 @@
 package com.inc.repository;
 
 import com.inc.entity.Ticket;
+import com.inc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    // ------------------- DERIVED QUERIES ------------------- //
+// ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to count how many tickets a user bought
+    Integer countAllByUserAccountId(Long id);
 
     //Write a derived query to list all tickets by specific email
+    List<Ticket> findAllByUserAccount_Email(String email);
 
     //Write a derived query to count how many tickets are sold for a specific movie
+    Integer countAllByMovieCinema_Movie_Name(String name);
 
     //Write a derived query to list all tickets between a range of dates
+    List<Ticket> findAllByDateTimeBetween(LocalDateTime dateTime1, LocalDateTime dateTime2);
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all tickets are bought from a specific user
+    @Query("SELECT t FROM Ticket t WHERE t.userAccount.id = ?1")
+    List<Ticket> fetchAllTicketByUserAccount(Long id);
 
     //Write a JPQL query that returns all tickets between a range of dates
+    @Query("SELECT t FROM Ticket t WHERE t.dateTime BETWEEN ?1 AND ?2")
+    List<Ticket> fetchAllTicketsBetweenRangeOfDates(LocalDateTime dateTime1, LocalDateTime dateTime2);
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count the number of tickets a user bought
+
 
     //Write a native query to count the number of tickets a user bought in a specific range of dates
 
