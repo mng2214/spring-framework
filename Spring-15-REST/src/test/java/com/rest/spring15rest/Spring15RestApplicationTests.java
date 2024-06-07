07c783958c84c8720e4cabf80897146465045c98
mng2214 <mng2214@gmail.com>
2024-06-06 21:18:16 -0500

    REST module added

---
+++ Spring-15-REST/src/test/java/com/rest/spring15rest/Spring15RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.spring15rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring15RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
