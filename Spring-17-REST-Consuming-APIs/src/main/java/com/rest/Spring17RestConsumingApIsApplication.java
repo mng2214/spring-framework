919319bfbeaf8176a0dbb8a6f375234fa4a63934
mng2214 <mng2214@gmail.com>
2024-06-08 11:49:42 -0500

    module 17 consuming api added

---
+++ Spring-17-REST-Consuming-APIs/src/main/java/com/rest/Spring17RestConsumingApIsApplication.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring17RestConsumingApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumingApIsApplication.class, args);
+    }
+
+}
---
+++ Spring-17-REST-Consuming-APIs/src/test/java/com/rest/Spring17RestConsumingApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring17RestConsumingApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
