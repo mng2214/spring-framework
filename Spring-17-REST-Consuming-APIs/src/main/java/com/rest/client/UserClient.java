bad77af0a3c2596c00071ccb6c1f41b2c474f5fa
mng2214 <mng2214@gmail.com>
2024-06-08 16:31:03 -0500

    Feign client added

---
+++ Spring-17-REST-Consuming-APIs/src/main/java/com/rest/client/UserClient.java
@@ -0,0 +1,16 @@
+package com.rest.client;
+
+import com.rest.dto.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(url = "https://jsonplaceholder.typicode.com/", name = "USER-CLIENT")
+public interface UserClient {
+
+    @GetMapping("/users")
+    List<User> getUsers();
+
+
+}
---
+++ Spring-17-REST-Consuming-APIs/src/main/java/com/rest/controller/Consume_FeignClient.java
@@ -0,0 +1,25 @@
+package com.rest.controller;
+
+import com.rest.client.UserClient;
+import com.rest.dto.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Consume_FeignClient {
+
+    private final UserClient userClient;
+
+    public Consume_FeignClient(UserClient userClient) {
+        this.userClient = userClient;
+    }
+
+    @GetMapping("/api/v1/users")
+    public ResponseEntity<ResponseWrapper> getUsers() {
+        return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved", userClient.getUsers()));
+    }
+
+
+}
