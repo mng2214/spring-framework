109898d1f6366fd5c2a892409cf8aab4d0a9f615
mng2214 <mng2214@gmail.com>
2024-06-08 12:34:27 -0500

    REST Template added

--- Spring-17-REST-Consuming-APIs/src/main/java/com/rest/Spring17RestConsumingApIsApplication.java
+++ Spring-17-REST-Consuming-APIs/src/main/java/com/rest/Spring17RestConsumingApIsApplication.java
@@ -2,6 +2,8 @@ package com.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring17RestConsumingApIsApplication {
@@ -10,4 +12,8 @@ public class Spring17RestConsumingApIsApplication {
         SpringApplication.run(Spring17RestConsumingApIsApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
---
+++ Spring-17-REST-Consuming-APIs/src/main/java/com/rest/controller/Consume_RestTemplate.java
@@ -0,0 +1,39 @@
+package com.rest.controller;
+
+import com.rest.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class Consume_RestTemplate {
+
+    private final String URI = "https://jsonplaceholder.typicode.com/users";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public Consume_RestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping
+    public User[] readAllUsers() {
+        ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI, User[].class);
+        return responseEntity.getBody();
+    }
+
+    @GetMapping("{id}")
+    public Object readUser(@PathVariable("id") Integer id) {
+        String URL = URI + "/{id}";
+        return restTemplate.getForObject(URL, Object.class, id);
+    }
+
+}
