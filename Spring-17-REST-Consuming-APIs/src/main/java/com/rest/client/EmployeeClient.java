f497a250c8372f43a209a21b556ee8b22767e69d
mng2214 <mng2214@gmail.com>
2024-06-08 17:00:18 -0500

    Feign client added

---
+++ Spring-17-REST-Consuming-APIs/src/main/java/com/rest/client/EmployeeClient.java
@@ -0,0 +1,13 @@
+package com.rest.client;
+
+import com.rest.dto.Employee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(url = "https://dummyapi.io", name = "EMPLOYEE-CLIENT")
+public interface EmployeeClient {
+
+    @GetMapping("/data/v1/user?limit=10")
+    Employee getEmployee(@RequestHeader("app-id") String id);
+}
--- Spring-17-REST-Consuming-APIs/src/main/java/com/rest/controller/Consume_FeignClient.java
+++ Spring-17-REST-Consuming-APIs/src/main/java/com/rest/controller/Consume_FeignClient.java
@@ -1,19 +1,24 @@
 package com.rest.controller;
 
+import com.rest.client.EmployeeClient;
 import com.rest.client.UserClient;
 import com.rest.dto.ResponseWrapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 public class Consume_FeignClient {
 
     private final UserClient userClient;
+    private final EmployeeClient employeeClient;
 
-    public Consume_FeignClient(UserClient userClient) {
+    public Consume_FeignClient(UserClient userClient, EmployeeClient employeeClient) {
         this.userClient = userClient;
+        this.employeeClient = employeeClient;
     }
 
     @GetMapping("/api/v1/users")
@@ -21,5 +26,10 @@ public class Consume_FeignClient {
         return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved", userClient.getUsers()));
     }
 
+    @GetMapping("/api/v1/employee")
+    public ResponseEntity<ResponseWrapper> getEmployee() {
+        return ResponseEntity
+                .ok(new ResponseWrapper("Retrieved", employeeClient.getEmployee("6298ebfecd0551211fce37a6")));
+    }
 
 }
