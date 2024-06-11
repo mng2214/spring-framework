18d5a3b65690f15532aa4d03ce877dc2042f0629
mng2214 <mng2214@gmail.com>
2024-06-10 22:04:59 -0500

    OPEN API Review

---
+++ Spring-18-REST-OpenApi3/src/main/java/com/rest/restjackson/controller/CinemaController.java
@@ -0,0 +1,31 @@
+package com.rest.restjackson.controller;
+
+
+import com.rest.restjackson.entity.Cinema;
+import com.rest.restjackson.repository.CinemaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "Cinema", description = "Cinema CRUD Operations")
+public class CinemaController {
+
+    private final CinemaRepository cinemaRepository;
+
+    public CinemaController(CinemaRepository cinemaRepository) {
+        this.cinemaRepository = cinemaRepository;
+    }
+
+
+    @GetMapping("/cinemas")
+    @Operation(summary = "Read all cinemas")
+    public List<Cinema> readAllCinemas(){
+        return cinemaRepository.findAll();
+    }
+}
--- Spring-18-REST-OpenApi3/src/test/java/com/rest/restjackson/Spring18RestOpenApi3ApplicationTests.java
+++ Spring-18-REST-OpenApi3/src/test/java/com/rest/restjackson/Spring18RestOpenApi3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring16RestJacksonApplicationTests {
+class Spring18RestOpenApi3ApplicationTests {
 
 	@Test
 	void contextLoads() {
