e63c8fe42410d10881608ed38968d5683baeef5a
mng2214 <mng2214@gmail.com>
2024-06-18 22:18:47 -0500

    OAuth20 added

---
+++ Spring-19-REST-Security-Keycloak-LicenseApp/src/test/java/com/cydeo/Spring19RestSecurityKeycloakLicenseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring19RestSecurityKeycloakLicenseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/controller/OrganizationController.java
@@ -0,0 +1,40 @@
+package com.cydeo.controller;
+
+import com.cydeo.model.Organization;
+import com.cydeo.service.OrganizationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+@RequestMapping(value = "/v1/organization")
+public class OrganizationController {
+
+    private final OrganizationService organizationService;
+
+    public OrganizationController(OrganizationService organizationService) {
+        this.organizationService = organizationService;
+    }
+
+    @RolesAllowed({"ADMIN","USER"})
+    @GetMapping("/{organizationId}")
+    public ResponseEntity<Organization> getOrganization(@PathVariable("organizationId") Long organizationId) throws Exception {
+        return ResponseEntity.ok(organizationService.findById(organizationId));
+    }
+
+    @RolesAllowed({"ADMIN","USER"})
+    @PostMapping
+    public ResponseEntity<Organization> createOrganization(@RequestBody Organization organization) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(organizationService.create(organization));
+    }
+
+    @RolesAllowed({"ADMIN"})
+    @DeleteMapping("/{organizationId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLicense(@PathVariable("organizationId") Long organizationId) {
+        organizationService.delete(organizationId);
+    }
+
+}
